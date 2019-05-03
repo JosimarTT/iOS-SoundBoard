032591ddaa2b74e866d3e143a093750cf8a8c636
Josimar Javier Tantahuilca Torres <josimar.tantahuilca@tecsup.edu.pe>
2019-05-03 17:53:34 -0500

    agregado botones tipo action

---
+++ SoundBoard/SoundViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SoundViewController.swift
+//  SoundBoard
+//
+//  Created by Josimar Javier Tantahuilca Torres on 3/05/19.
+//  Copyright © 2019 Tecsup. All rights reserved.
+//
+
+import UIKit
+
+class SoundViewController: UIViewController {
+
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBAction func recordTapped(_ sender: Any) {
+    }
+    
+    @IBAction func playTapped(_ sender: Any) {
+    }
+   
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
