84da2b23ba879e8e65a50182294b8c3c7fa9196c
Josimar Javier Tantahuilca Torres <josimar.tantahuilca@tecsup.edu.pe>
2019-05-04 15:59:47 -0500

    agregado funcion record

--- SoundBoard/SoundViewController.swift
+++ SoundBoard/SoundViewController.swift
@@ -7,13 +7,62 @@
 //
 
 import UIKit
+import AVFoundation
 
 class SoundViewController: UIViewController {
 
     @IBOutlet weak var recordButton: UIButton!
     @IBOutlet weak var nameTextField: UITextField!
+    var audioRecorder: AVAudioRecorder?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupRecorder()
+        
+    }
+    
+    func setupRecorder() {
+        do {
+            // crear una sesion de audio
+            let session = AVAudioSession.sharedInstance()
+            try session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+            try session.overrideOutputAudioPort(.speaker)
+            try session.setActive(true)
+            
+            // creando una direccion para el archivo de audio
+            let basePath: String = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+            let pathComponents = [basePath, "audio.m4a"]
+            let audioURL = NSURL.fileURL(withPathComponents: pathComponents)!
+            print("**********************")
+            print(audioURL)
+            print("**********************")
+            
+            // crear opciones para el grabador de audio
+            var settings: [String: AnyObject] = [:]
+            settings[AVFormatIDKey] = Int(kAudioFormatMPEG4AAC) as AnyObject?
+            settings[AVSampleRateKey] = 44100.0 as AnyObject?
+            settings[AVNumberOfChannelsKey] = 2 as AnyObject?
+            
+            // crear el objecto de grabacion de audio
+            audioRecorder = try AVAudioRecorder(url: audioURL, settings: settings)
+            audioRecorder!.prepareToRecord()
+        } catch let error as NSError {
+            print(error)
+        }
+    }
     
     @IBAction func recordTapped(_ sender: Any) {
+        if audioRecorder!.isRecording {
+            // detener la grabacion
+            audioRecorder?.stop()
+            // cambiar el texto del boton grabar
+            recordButton.setTitle("Record", for: .normal)
+        } else {
+            // empezar a grabar
+            audioRecorder?.record()
+            // cambiar el titulo del boton a detener
+            recordButton.setTitle("Stop", for: .normal)
+        }
     }
     
     @IBAction func playTapped(_ sender: Any) {
@@ -21,16 +70,9 @@ class SoundViewController: UIViewController {
    
     @IBAction func addTapped(_ sender: Any) {
     }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 }
